ea53def5e9d6d1a54b1251ed309d4f91a7b54e68
Rodrigo Noschang <rodrigo@hotmail.com>
2022-10-19 12:21:07 -0300

    change env database uri

--- app/configs/env_configs.py
+++ app/configs/env_configs.py
@@ -7,5 +7,5 @@ load_dotenv()
 def init_app(app: Flask):
     app.config['JSON_SORT_KEYS'] = False
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['DATABASE_URL'] = environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
     app.config['SECRET_KEY'] = environ.get('SECRET_KEY')
