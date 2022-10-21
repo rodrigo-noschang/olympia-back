a0ff599e956c4cca88718732b9b5ec92cd81ee85
Rodrigo Noschang <rodrigo@hotmail.com>
2022-10-21 12:28:52 -0300

    removing decodeError

--- app/controller/token_required_controller.py
+++ app/controller/token_required_controller.py
@@ -1,6 +1,6 @@
 from flask import request
 import jwt
-from jwt.exceptions import DecodeError
+# from jwt.exceptions import DecodeError
 from os import environ
 from datetime import datetime
 from app.exc.exc import NonAuthenticated, SessionExpired
@@ -8,15 +8,15 @@ from app.exc.exc import NonAuthenticated, SessionExpired
 def token_required():
     token = request.headers.get('Authorization').split(' ')[1]
     
-    try:    
-        decoded_token = jwt.decode(token, environ.get('SECRET_KEY'), algorithms=['HS256'])
-        expiration_time_string = decoded_token.get('expiration')
-        expiration_time = datetime.strptime(expiration_time_string, "%Y-%m-%d %H:%M:%S.%f")
-        current_time = datetime.utcnow()
+    # try:    
+    decoded_token = jwt.decode(token, environ.get('SECRET_KEY'), algorithms=['HS256'])
+    expiration_time_string = decoded_token.get('expiration')
+    expiration_time = datetime.strptime(expiration_time_string, "%Y-%m-%d %H:%M:%S.%f")
+    current_time = datetime.utcnow()
 
-        if current_time > expiration_time:
-            raise SessionExpired
+    if current_time > expiration_time:
+        raise SessionExpired
 
-        return decoded_token
-    except DecodeError as err:
-        raise NonAuthenticated
+    return decoded_token
+    # except DecodeError as err:
+    #     raise NonAuthenticated
